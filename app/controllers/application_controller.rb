aa2832f86707809480f4d092bfd162a1e5449dcf
Alexandra Fren <alexandra.fren@gmail.com>
2019-01-24 20:26:13 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,8 +8,12 @@ class ApplicationController < ActionController::Base
   private
 
   def authenticate_user
+    client_id = "96a4eb17311dab530ea3"
+    github_url = "https://github.com/login/oauth/authorize?client_id=#{client_id}"
+    redirect_to github_url unless logged_in?
   end
 
   def logged_in?
+    !!session[:token]
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,5 +2,14 @@ class SessionsController < ApplicationController
   skip_before_action :authenticate_user
 
   def create
+    client_id = '96a4eb17311dab530ea3'
+    client_secret = '2bd22e593a540174830fa6899ddc7aae445b9882'
+    response = Faraday.post "https://github.com/login/oauth/access_token" do |r|
+      r.body = { 'client_id': client_id, 'client_secret': client_secret, 'code': params[:code] }
+      r.headers['Accept'] = 'application/json'
+    end
+    body = JSON.parse(response.body)
+    session[:token] = body["access_token"]
+    redirect_to root_path
   end
 end
