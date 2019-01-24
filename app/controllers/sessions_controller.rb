7d6826e2f30e1bbbd2d8cb9fdb9c85d96693120c
Alexandra Fren <alexandra.fren@gmail.com>
2019-01-24 20:36:19 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   skip_before_action :authenticate_user
 
   def create
-    client_id = '96a4eb17311dab530ea3'
+    client_id = ENV[GITHUB_CLIENT_ID]
     client_secret = '2bd22e593a540174830fa6899ddc7aae445b9882'
     response = Faraday.post "https://github.com/login/oauth/access_token" do |r|
       r.body = { 'client_id': client_id, 'client_secret': client_secret, 'code': params[:code] }
